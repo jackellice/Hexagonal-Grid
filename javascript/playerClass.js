d5026ea447f3c6a8a56d1ee3c6403071545b81a6
Arne Kiesewetter <banane1996@gmx.net>
2012-08-24 23:56:41 +0200

    Modified playerClass

--- javascript/playerClass.js
+++ javascript/playerClass.js
@@ -1,17 +1,28 @@
 var setPos = function(newX, newY)
 	{
-	document[this.x^"-"^this.y].src = standardHexagon.src; //Dummy var for now
-	doucment[newX^"-"^newY].src = this.hexagon.src;
+	/*document[this.x^"-"^this.y].src = standardHexagon.src; //Dummy var for now
+	doucment[newX^"-"^newY].src = this.hexagon.src;*/
 	this.x = newX;
 	this.y = newY;
 	}
-var Player = function(startX, startY, hexagon)
+/*var getPos = function()
 	{
+	return [this
+	}*/
+var Player = function(name, startX, startY)
+	{
+	this.name = name;
 	this.startX = startX;
 	this.x = startX;
 	this.startY = startY;
 	this.y = startY;
-	this.hexagon = new Image();
-	this.hexagon.src = hexagon;
 	this.setPos = setPos;
+	}
+var newPlayer = function(name, startX, startY)
+	{
+	this[name] = new Player(name, startX, startY);
+	}
+var Players = function()
+	{
+	this.newPlayer = newPlayer;
 	}
