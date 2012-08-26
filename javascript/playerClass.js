51095fdec0d4fc1c56aece5bb30b028c72a21b3c
jackellice <jackellice@hotmail.com>
2012-08-26 21:29:09 +0800

    Propulsion fully works. added your spacing

--- javascript/playerClass.js
+++ javascript/playerClass.js
@@ -16,30 +16,31 @@ var Player = function(startX, startY, src, canvas2d)
 		};
 	this.setPos = setPos;
 	this.momentum = [0,0,0];
-	this.planetGravityPull=0;
-	this.move=function(x,y,z){
-	var upDown=y*104+z*52+x*52;
-	var leftRight=x*90+z*90;
-	if(this.y+upDown>1000||this.x+leftRight>900||this.y+upDown<5||this.x+leftRight<5)
+	this.planetGravityPull = 0;
+	this.move=function(x, y, z)
+	{
+	this.momentum[0] += x;
+	this.momentum[1] += y;
+	this.momentum[2] += z;
+	var upDown=this.momentum[1] * 104+this.momentum[2] * 52 + this.momentum[0] * 52;
+	var leftRight=this.momentum[0] * 90-this.momentum[2] * 90;
+	if(this.y+upDown > 1000 ||this.x + leftRight>900 || this.y+upDown<5 || this.x + leftRight < 5)
 		{
 		console.log("you can't go there");
-		}
-	else
+		};
 		{
-		canvas2d.drawImage(board.background,this.x,this.y,120,104);
-		canvas2d.drawImage(this.token,this.x+leftRight,this.y+upDown,120,104);
-		this.x+=leftRight;
-		this.y+=upDown;
-		}};
-	this.turn=function(x,y,z)
+		canvas2d.drawImage(board.background, this.x, this.y,120, 104);
+		canvas2d.drawImage(this.token, this.x+leftRight, this.y+upDown, 120, 104);
+		this.x += leftRight;
+		this.y += upDown;
+		}
+	};
+	this.turn=function(x, y, z)
 	{
-	this.momentum[0]+=x;
-	this.momentum[1]+=y
-	this.momentum[2]+=z;
-	this.move(x,y,z);
-	this.crashPlanet = function(theCraft,Planetx,Planety)
+	this.move(x, y, z);
+	this.crashPlanet = function(theCraft, Planetx, Planety)
 	{
-	if(theCraft[0]===Planetx&& Planety===theCraft[1])
+	if(theCraft[0] === Planetx && Planety === theCraft[1])
 		{
 		console.log("You've Crashed! Game over");
 		}
