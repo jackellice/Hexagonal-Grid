c6ac7d91e9190fd6487cd994fa025bef8b5e2e87
jackellice <jackellice@hotmail.com>
2012-08-28 02:31:23 +0800

    …

--- javascript/planetClass.js
+++ javascript/planetClass.js
@@ -10,18 +10,19 @@ var updateGravityFields = function()
 var Planet = function(x, y, src, canvas2d)
 	{
 	this.token = new Image(); this.token.src = src;
-	this.token.onload = function()
-		{
-		var c = 26;
-		if(x % 2 == 0)
+	c = 26;
+	if(x % 2 == 0)
 			{
 			c = 0;
 			}
+	this.token.onload = function()
+		{
+		
 		canvas2d.drawImage(this, 45 * x +5-90, 5 + 52 * y + c-52, 60, 52);
 		}
 	this.coordinate = [];
-	this.coordinate.x = x;
-	this.coordinate.y = y;
+	this.coordinate.x = 45 * x +5-90;
+	this.coordinate.y = 5 + 52 * y+ c;
 	this.gravityFields = [];
 	this.updateGravityFields = updateGravityFields;
 	}
